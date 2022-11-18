c7400b06ab14febfcede1b55efcf7e7a31162687
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-11-18 21:08:00 +0430

    Factorial Problem defined.

---
+++ algorithms/factorial.js
@@ -0,0 +1,14 @@
+// Problem
+// Given an integer "n", find the factorial of that integer.
+// In mathematics, the factorial of a non-negative integer "n", denoted n!, is the product of all positve integers less than or equal to "n".
+// Fatorial of zero is 1.
+// Fatorial(4) = 4*3*2*1 = 24
+// Fatorial(5) = 5*4*3*2*1 = 120
+
+function factorial(n) {
+  let facto = 1;
+
+
+}
+
+console.log(factorial(2));
