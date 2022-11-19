66fc88e5d4adec0bbf4c37e79d583e2af554ff1a
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-11-19 21:07:45 +0430

    Prime Number algorithm solved

--- algorithms/primeNumber.js
+++ algorithms/primeNumber.js
@@ -1,5 +1,21 @@
 // Problem:
-// Given a natural number "n", detemine if the number is prime or not.
-// A prime number is a natural number greater thean 1 that is not a product of two smaller natural numbers.
+// Given a natural number "n", determine if the number is prime or not.
+// A prime number is a natural number greater then 1 that is not a product of two smaller natural numbers.
 // isPrime(5) = true (1*5 or 5*1)
 // isPrime(4) = false (1*4 or 2*2 or 4*1)
+
+function isPrime(n) {
+  if(n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(1)); // false
+console.log(isPrime(5)); // true
+console.log(isPrime(4)); // false
