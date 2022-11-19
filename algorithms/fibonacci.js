b64adc21b7276d533fb9688f766d9a2d7786ea05
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-11-19 20:38:55 +0430

    fibonacci problem description

--- algorithms/fibonacci.js
+++ algorithms/fibonacci.js
@@ -1,3 +1,11 @@
+// Problem:
+// Give a number "n", find the first "n" elements of the Fibonacci sequence.
+// In mathematice, the Fibonacci sequence is a sequence in which each number is the sum of two preceding ones.
+// The first two numbers in the sequence are 0 and 1.
+// fibonacci(2) = [0.1]
+// fibonacci(3) = [0,1,1]
+// fibonacci(7) = [0,1,1,2,3,5,8]
+
 function fibonacci(n) {
   let fib = [0, 1];
 	for(let i = 2; i < n; i++) {
