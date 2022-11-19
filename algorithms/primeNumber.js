caa875267738ffda280a278c72da6923d9b829be
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-11-19 20:55:57 +0430

    Prime Number algorithm description

---
+++ algorithms/primeNumber.js
@@ -0,0 +1,5 @@
+// Problem:
+// Given a natural number "n", detemine if the number is prime or not.
+// A prime number is a natural number greater thean 1 that is not a product of two smaller natural numbers.
+// isPrime(5) = true (1*5 or 5*1)
+// isPrime(4) = false (1*4 or 2*2 or 4*1)
